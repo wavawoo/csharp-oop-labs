b6f83c343c672af4607741c029d079d548c950fd
why seven? <kavvxxz@gmail.com>
2025-10-22 20:26:15 +0300

    Create Program.cs

---
+++ Lab3-DateConversion/Exercise1-Basic/Program.cs
@@ -0,0 +1,49 @@
+using System;
+
+enum MonthName
+{
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
+
+class WhatDay
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a day number between 1 and 365:");
+        string line = Console.ReadLine();
+        int dayNum = int.Parse(line);
+
+        int monthNum = 0;
+
+        foreach (int daysInMonth in DaysInMonths)
+        {
+            if (dayNum <= daysInMonth)
+            {
+                break;
+            }
+            else
+            {
+                dayNum -= daysInMonth;
+                monthNum++;
+            }
+        }
+
+        MonthName temp = (MonthName)monthNum;
+        string monthName = temp.ToString();
+
+        Console.WriteLine("{0} {1}", monthName, dayNum);
+    }
+
+    static int[] DaysInMonths = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+}
