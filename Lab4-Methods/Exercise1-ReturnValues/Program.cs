5c081c1d2346a48f11272dc383b59eb339ef8d08
why seven? <kavvxxz@gmail.com>
2025-10-22 20:35:50 +0300

    Create Program.cs

---
+++ Lab4-Methods/Exercise1-ReturnValues/Program.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Utils
+{
+    public static int Greater(int a, int b)
+    {
+        return a > b ? a : b;
+    }
+}
+
+class Test
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter first integer number:");
+        int x = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter second integer number:");
+        int y = int.Parse(Console.ReadLine());
+
+        int greater = Utils.Greater(x, y);
+        Console.WriteLine($"The greater number is: {greater}");
+    }
+}
