7d5eba7bb5c86230aa6c7668c0fc2aa82558201d
why seven? <kavvxxz@gmail.com>
2025-10-22 20:52:59 +0300

    Create CreateAccount.cs

---
+++ Lab6-ClassesEncapsulation/Exercise1-StructToClass/CreateAccount.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace FileDetails
+{
+    class CreateAccount
+    {
+        static void Main()
+        {
+            BankAccount berts = NewBankAccount();
+            Write(berts);
+
+            BankAccount freds = NewBankAccount();
+            Write(freds);
+        }
+
+        static BankAccount NewBankAccount()
+        {
+            BankAccount created = new BankAccount();
+
+            Console.Write("Enter the account number   : ");
+            long number = long.Parse(Console.ReadLine());
+
+            Console.Write("Enter the account balance! : ");
+            decimal balance = decimal.Parse(Console.ReadLine());
+
+            created.Populate(number, balance);
+
+            /*
+            created.accNo = number;
+            created.accBal = balance;
+            created.accType = AccountType.Checking;
+            */
+
+            return created;
+        }
+
+        static void Write(BankAccount toWrite)
+        {
+            Console.WriteLine("Account number is {0}", toWrite.Number());
+            Console.WriteLine("Account balance is {0}", toWrite.Balance());
+            Console.WriteLine("Account type is {0}", toWrite.Type());
+        }
+    }
+}
