ebd833ba097f3a344db202a8937d147dfad5c0d7
why seven? <kavvxxz@gmail.com>
2025-10-22 21:06:19 +0300

    Create IPrintable.cs

---
+++ Lab7-ReferenceTypes/Exercise5-InterfaceUsage/IPrintable.cs
@@ -0,0 +1,10 @@
+namespace Utils
+{
+    using System;
+
+    // Интерфейс для объектов, поддерживающих печать
+    interface IPrintable
+    {
+        void Print();  // Метод для вывода информации об объекте
+    }
+}
