de575b3634038c0961f22ae6ba5ea2b36daa8656
why seven? <kavvxxz@gmail.com>
2025-10-22 20:28:44 +0300

    Create Program.cs

---
+++ Lab3-DateConversion/Exercise3-AdditionalFeatures/Program.cs
@@ -0,0 +1,88 @@
+using System;
+
+enum MonthName
+{
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
+
+class WhatDay
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Enter the year:");
+            string yearLine = Console.ReadLine();
+            int yearNum = int.Parse(yearLine);
+
+            bool isLeapYear = (yearNum % 4 == 0);
+            int maxDayNum = isLeapYear ? 366 : 365;
+
+            Console.WriteLine($"Enter a day number between 1 and {maxDayNum}:");
+            string line = Console.ReadLine();
+            int dayNum = int.Parse(line);
+
+            if (dayNum < 1 || dayNum > maxDayNum)
+            {
+                throw new ArgumentOutOfRangeException("Day out of Range");
+            }
+
+            int monthNum = 0;
+
+            if (isLeapYear)
+            {
+                foreach (int daysInMonth in DaysInLeapMonths)
+                {
+                    if (dayNum <= daysInMonth)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        dayNum -= daysInMonth;
+                        monthNum++;
+                    }
+                }
+            }
+            else
+            {
+                foreach (int daysInMonth in DaysInMonths)
+                {
+                    if (dayNum <= daysInMonth)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        dayNum -= daysInMonth;
+                        monthNum++;
+                    }
+                }
+            }
+
+            MonthName temp = (MonthName)monthNum;
+            string monthName = temp.ToString();
+
+            Console.WriteLine("{0} {1}", monthName, dayNum);
+        }
+        catch (Exception caught)
+        {
+            Console.WriteLine("Error: " + caught.Message);
+        }
+    }
+
+    static int[] DaysInMonths = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+    static int[] DaysInLeapMonths = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+}
