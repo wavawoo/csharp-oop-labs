8b04849f23e67562567db3e3ec107b322d054749
why seven? <kavvxxz@gmail.com>
2025-10-22 20:56:35 +0300

    Create BankAccount.cs

---
+++ Lab6-ClassesEncapsulation/Exercise3-DepositWithdraw/BankAccount.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace FileDetails
+{
+    class BankAccount
+    {
+        private long accNo;
+        private decimal accBal;
+        private AccountType accType;
+        public void Populate(decimal balance)
+        {
+            accNo = NextNumber();
+            accBal = balance;
+            accType = AccountType.Checking;
+        }
+
+        public long Number() { return accNo; }
+        public decimal Balance() { return accBal; }
+        public string Type() { return accType.ToString(); }
+
+        private static long nextAccNo = 123;
+        private static long NextNumber() { return nextAccNo++; }
+
+        public decimal Deposit(decimal amount)
+        {
+            accBal += amount;
+            return accBal;
+        }
+        public bool Withdraw(decimal amount)
+        {
+            accBal -= amount;
+            return true;
+        }
+
+    }
+}
